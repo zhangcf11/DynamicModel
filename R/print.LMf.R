6d1d8b89c719b0078dcc18f25f39384b422faf2d
zhangcf11 <117132947+zhangcf11@users.noreply.github.com>
2025-03-19 17:39:52 +0800

    Delete R directory

--- R/print.LMf.R
+++
@@ -1,7 +0,0 @@
-#' Title output result of function "LMf"
-#'
-#' @param object an object inheriting from class LMf
-#' @export
-print.LMf<-function(object,...){
-  print(object$Model,...)
-}
--- R/print.VS_LM.R
+++
@@ -1,7 +0,0 @@
-#' Title output result of function "VS_LM"
-#'
-#' @param object an object inheriting from class VS_LM
-#' @export
-print.VS_LM<-function(object,...){
-  print(object$Model,...)
-}
